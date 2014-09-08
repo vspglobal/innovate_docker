723d332e3d5879ca3b2eb9f65dcf97e4207c6331
peteja <peteja@vsp.com>
2014-09-07 20:17:30 -0700

    added innovate_service project

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 
 docker build --rm=true -t="innovate_base" ./innovate_base/
 docker build --rm=true -t="innovate_java" ./innovate_java/
-docker build --rm=true -t="innovate_tomcat" ./innovate_tomcat/
-docker build --rm=true -t="innovate_sampleapp" ./innovate_sampleapp/
+docker build --rm=true -t="innovate_service" ./innovate_service/
+#docker build --rm=true -t="innovate_tomcat" ./innovate_tomcat/
+#docker build --rm=true -t="innovate_sampleapp" ./innovate_sampleapp/
 
