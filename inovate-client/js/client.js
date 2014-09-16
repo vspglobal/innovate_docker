1c888d7265df5880e14b4c02327c1adbf3838da0
John Petersen <john.petersen@vsp.com>
2014-09-15 17:18:06 -0700

    client stub

---
+++ inovate-client/js/client.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function doAllTheWork() {
+
+	console.log("doing all the work");
+
+	<!-- Get and display/log the version of etcd being accessed. -->
+	<!-- http://127.0.0.1:4001/version -->
+
+	<!-- if we fail to find etcd pop up a dialog asking for the server -->
+
+	<!-- Call the long polling function to process service instances. -->
+	queryServiceList(false);
+}
+
+function queryServiceList(wait) {
+
+	<!-- http://127.0.0.1:4001/v2/keys/services?wait=true -->
+	$.get( "data/services.json", function( data ) {
+		  var svcArray = data.node.value;
+		  if (svcArray == null) return;
+		  var svcCount = svcArray.length;
+			for (var i = 0; i < svcCount; i++) {
+				// TODO query the service to get the state
+				var state = {};
+		  	buildSvcDisplay(svcArray[i].name, svcArray[i].url, state);
+		  }
+		}, "json" );
+	
+}
+
+function buildSvcDisplay(name, url, state) {
+	$("#all_the_things").append( "Name: " + name + " is at " + url);
+}
