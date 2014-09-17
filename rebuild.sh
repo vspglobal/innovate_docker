78e6d5276ed66ec38f8f89358f5337f8b5b3daf4
peteja <peteja@vsp.com>
2014-09-16 17:11:24 -0700

    added meta service to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,7 @@ docker build --rm=true -t="innovate_base" ./innovate_base/
 docker build --rm=true -t="innovate_java" ./innovate_java/
 docker build --rm=true -t="innovate_service:v1.0" ./innovate_service_v1.0/
 docker build --rm=true -t="innovate_service:v2.0" ./innovate_service_v2.0/
+docker build --rm=true -t="innovate_meta_service:v1.0" ./innovate_meta_service/
 
 #docker build --rm=true -t="innovate_tomcat" ./innovate_tomcat/
 #docker build --rm=true -t="innovate_sampleapp" ./innovate_sampleapp/
