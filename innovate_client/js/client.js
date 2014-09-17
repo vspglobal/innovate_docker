660851c3c30dcc7497aa9c2b1f92e0364b167523
Casey Lee <casele@vsp.com>
2014-09-16 22:11:48 -0700

    fixed

--- innovate_client/js/client.js
+++ innovate_client/js/client.js
@@ -2,7 +2,7 @@
 
 function doAllTheWork() {
 
-	console.log("doing all the work");
+//	console.log("doing all the work");
 
 	<!-- Get and display/log the version of etcd being accessed. -->
 	<!-- http://127.0.0.1:4001/version -->
@@ -16,39 +16,39 @@ function doAllTheWork() {
 function queryServiceList(wait) {
 
 	$.get( "http://54.244.91.57:8000/services", function( data ) {
-	
+
 		var svcArray = data.node.nodes;
 		if (svcArray == null) return;
-		
+
 		var nodeMap = {};
 		var svcCount = svcArray.length;
 		for (var i = 0; i < svcCount; i++) {
 			var nodeEntry = JSON.parse( svcArray[i].value );
-			
-			var key = nodeEntry.host + ":" + nodeEntry.port + ":" + nodeEntry.version;
-			
-			nodeMap[key] = nodeEntry;
-		}  
-		
+
+			var k = nodeEntry.host + ":" + nodeEntry.port + ":" + nodeEntry.version;
+
+			nodeMap[k] = nodeEntry;
+		}
+
 		var keyArray = Object.keys(nodeMap).sort();
-		
+
 		console.log( keyArray );
-		
+
 		var p = d3.select("#all_the_things")
 			.selectAll("div")
-			.data( keyArray );
-					
+			.data( keyArray, function(key) {return key} );
+
 		// Enter…
 		p.enter()
 			.append("div")
 			.attr("class", "dockerNode")
 			.attr("id",
-				function( key ) { 
+				function( key ) {
 					return key;
 				}
 			)
 			.html(
-				function( key ) { 
+				function( key ) {
 					var n = nodeMap[key];
 					buildOneNode(key, n, this);
 					return "<img style='background-color: #ffffff' src='https://s3-us-west-2.amazonaws.com/innovate-day/docker/loading.gif' height='120' width='120'/>";
@@ -58,9 +58,9 @@ function queryServiceList(wait) {
 		// Exit…
 		p.exit().remove();
 
-		
+
 	}, "json" );
-	
+
 }
 
 function buildOneNode(key, nodeEntry, dockerNode) {
@@ -68,7 +68,7 @@ function buildOneNode(key, nodeEntry, dockerNode) {
 	$.get( nodeEntry.url, function( data ) {
 
 		dockerNode.innerHTML = "<img src='" + data.imageUrl + "'/><br>" + data.ip + "<br>version " + data.version;
-		
+
 	}, "json" ).fail( function() {
 		setTimeout( function() {
 			buildOneNode(key, nodeEntry, dockerNode);
